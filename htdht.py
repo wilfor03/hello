efb6a9e20b9866f64fd1a3710641625c11caf233
wilfor03 <90191416+wilfor03@users.noreply.github.com>
2022-01-17 13:32:20 +0100

    🎉

---
+++ htdht.py
@@ -0,0 +1,1 @@
+input('vad är kolokan')
---
+++ if.py
@@ -0,0 +1,10 @@
+svar = input ('name ')
+if svar == 'noel':
+     print('noel dö')
+elif svar == 'linus':
+     print('linus rerard')
+elif svar == 'william G':
+     print ('william G är trött')
+     L = input ('hur myket monster har william G drukit ')
+     if L == float <= 4.0:
+         print ('HAN ÄR VÄLDIGT HÖG')
